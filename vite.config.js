02c704fe974a7fb7b31d48056de8b3dd638d3a76
Santhosh <santhoshthamodharan2008@gmail.com>
2026-03-15 22:57:40 +0530

    Fixing base path for white screen

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/santhoshlegacy/portfolio/', // REPLACE 'your-repo-name' with your actual GitHub repo name
 })
